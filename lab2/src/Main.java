4fa4e16aa34896d5f499aab251d15bc996eb4105
Damir Ismagilov <damiron123@gmail.com>
2018-10-26 19:18:38 +0300

    add try with resourses instead throwing checked exception

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -4,31 +4,35 @@ import java.util.Set;
 import java.util.*;
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("ListOfEdges.txt"));
-        Map<Integer, List<EdgeDirection>> listOfEdges = new HashMap<>();
-        Set<Integer> numberOfVertexes = new HashSet<>();
-        while (scanner.hasNext()) {
-            int src, dst, weight;
-            src = scanner.nextInt();
-            dst = scanner.nextInt();
-            numberOfVertexes.add(src);
-            numberOfVertexes.add(dst);
-            weight = scanner.nextInt();
-            List<EdgeDirection> list;
-            if (listOfEdges.containsKey(src)) {
-                list = listOfEdges.get(src);
-                list.add(new EdgeDirection(dst, weight));
+    public static void main(String[] args) {
+        String fileName = "ListOfEdges.txt";
+        try (Scanner scanner = new Scanner(new File(fileName));){
+            Map<Integer, List<EdgeDirection>> listOfEdges = new HashMap<>();
+            Set<Integer> numberOfVertexes = new HashSet<>();
+            while (scanner.hasNext()) {
+                int src, dst, weight;
+                src = scanner.nextInt();
+                dst = scanner.nextInt();
+                numberOfVertexes.add(src);
+                numberOfVertexes.add(dst);
+                weight = scanner.nextInt();
+                List<EdgeDirection> list;
+                if (listOfEdges.containsKey(src)) {
+                    list = listOfEdges.get(src);
+                    list.add(new EdgeDirection(dst, weight));
+                }
+                else {
+                    list = new LinkedList<>();
+                    list.add(new EdgeDirection(dst, weight));
+                }
+                listOfEdges.put(src, list);
             }
-            else {
-                list = new LinkedList<>();
-                list.add(new EdgeDirection(dst, weight));
+            List<Integer> paths = Dijkstra.findBestPaths(listOfEdges, numberOfVertexes.size());
+            for (int i = 0; i < paths.size(); ++i) {
+                System.out.println((i + 1) + " " + paths.get(i));
             }
-            listOfEdges.put(src, list);
-        }
-        List<Integer> paths = Dijkstra.findBestPaths(listOfEdges, numberOfVertexes.size());
-        for (int i = 0; i < paths.size(); ++i) {
-            System.out.println((i + 1) + " " + paths.get(i));
+        } catch (FileNotFoundException ex) {
+            System.out.println("File is not found " + fileName);
         }
     }
 }
