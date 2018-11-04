0628d10f69aa9f0756a54e3aa18bd04b345e201b
Damir Ismagilov <damiron123@gmail.com>
2018-11-04 13:15:22 +0300

    add methods for Matrix.java

--- lab3/src/Matrix.java
+++ lab3/src/Matrix.java
@@ -1,3 +1,5 @@
+import javafx.util.Pair;
+
 import java.util.ArrayList;
 
 public class Matrix {
@@ -5,12 +7,44 @@ public class Matrix {
     private int m;
     private ArrayList<ArrayList<Integer>> matrix;
 
-    Matrix(int n, int m){
+    Matrix(int n, int m) {
         this.n = n;
         this.m = m;
         this.matrix = new ArrayList<>(n);
-        for (int i = 0; i < n; i++) {
-            matrix.add(new ArrayList<>(m));
+    }
+
+    void addRow(int i, ArrayList<Integer> row) {
+        if (i >= n || row.size() != m) {
+            throw new IndexOutOfBoundsException();
         }
+        if (i < matrix.size()) {
+            matrix.set(i, row);
+        }
+        else {
+            matrix.add(row);
+        }
+    }
+
+    void addRow(ArrayList<Integer> row) {
+        if (row.size() != m) {
+            throw new IndexOutOfBoundsException();
+        }
+        matrix.add(row);
+    }
+
+    void addColumn(ArrayList<Integer> column) {
+        if (column.size() != matrix.size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        for (int i = 0; i < column.size(); ++i) {
+            matrix.get(i).add(column.get(i));
+        }
+    }
+
+    ArrayList<Integer> getRow(int i) {
+        if (i >= matrix.size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        return matrix.get(i);
     }
 }
